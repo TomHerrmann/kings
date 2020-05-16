8630692bff181da490906dbe0b3134107eb6965e
Tom Herrmann <tomherrmanndev@gmail.com>
2020-05-16 14:01:53 -0400

    Styling and cleanup

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,10 +5,9 @@ import faceDownCard from '../../assets/facedowncard.png';
 import { rulesStore } from '../utils/enums';
 
 const Game = ({ cardsRemaining, currentCard, displayGif }) => {
-  console.log('displayGif', displayGif);
   return (
     <div className="game-container" className="game-container">
-      <div className="game-left">
+      <div className="game-half">
         <div>
           {currentCard ? (
             <img src={currentCard.image} alt={currentCard.code} />
@@ -18,13 +17,15 @@ const Game = ({ cardsRemaining, currentCard, displayGif }) => {
         </div>
         <div>cards remaining: {cardsRemaining}</div>
       </div>
-      <div className="game-right">
+      <div className="game-half">
         <div className="prompt-container">
           <strong>{currentCard ? rulesStore[currentCard.value] : 'click draw card to play'}</strong>
         </div>
         <div className="gif-container">
           <iframe src={displayGif}></iframe>
-          <p>"Powered By GIPHY"</p>
+          <p>
+            <small>"Powered By GIPHY"</small>
+          </p>
         </div>
       </div>
     </div>
